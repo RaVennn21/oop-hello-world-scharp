182072deaa67436a22aca2c06f01b875ac817a8a
RaVennn21 <vian21okta@gmail.com>
2025-03-03 12:08:25 +0700

    feat: add Hello printer

---
+++ src/HelloWorld/HelloLib/HelloPrinter.cs
@@ -0,0 +1,10 @@
+﻿namespace HelloLib
+{
+    public class HelloPrinter
+    {
+        public void sayHello(string massage)
+        {
+            Console.WriteLine("Hello: " + massage);
+        }
+    }
+}
--- src/HelloWorld/HelloWorld/Program.cs
+++ src/HelloWorld/HelloWorld/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace HelloWorld
+﻿using HelloLib;
+
+namespace HelloWorld
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Enter your name ");
+            string? name = Console.ReadLine();
+            HelloPrinter printer = new HelloPrinter();
+
+            if (name == null)
+            {
+                Console.WriteLine("Please write your name la!!!");
+            }
+
+            printer.sayHello(name);
         }
     }
 }
